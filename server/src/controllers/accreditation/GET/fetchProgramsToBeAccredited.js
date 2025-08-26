05c0888f258315ed1adbc308b8c3a36485b80ae9
Joe Francis Casinillo <casinillojoefrancis@gmail.com>
2025-08-27 00:03:49 +0800

    feat: implement fetching of programs to be accredited with database query integration

---
+++ server/src/controllers/accreditation/GET/fetchProgramsToBeAccredited.js
@@ -0,0 +1,20 @@
+import { getProgramsToBeAccredited } from "../../../models/program-to-accredit/GET/getProgramToBeAccredited.js";
+
+const fetchProgramsToBeAccredited = async (req, res) => {
+  try {
+    const programs = await getProgramsToBeAccredited();
+    res.status(200).json({
+      success: true,
+      data: programs
+    });
+
+  } catch (error) {
+    console.error('Controller has error in fetching program: ', error);
+    res.status(500).json({
+      success: false,
+      message: 'Failed to fetch programs to be accredited.'
+    });
+  }
+};
+
+export default fetchProgramsToBeAccredited;
--- server/src/routes/accreditationRoute.js
+++ server/src/routes/accreditationRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { addProgramtoAccredit } from '../controllers/program/programToAccreditController.js';
 import { addLevel } from '../controllers/accreditation-level/levelController.js';
+import fetchProgramsToBeAccredited from '../controllers/accreditation/GET/fetchProgramsToBeAccredited.js';
 
 const accreditationRouter = express.Router();
 
 accreditationRouter.post('/add-accreditation-level', addLevel);
 accreditationRouter.post('/add-program-to-accredit', addProgramtoAccredit);
+accreditationRouter.get('/fetch-program-to-be-accredited', fetchProgramsToBeAccredited);
 
 export default accreditationRouter;
